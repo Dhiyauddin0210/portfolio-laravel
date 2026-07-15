4a9f4ab785206d680edb2fa9b6e0ccf7dc49559d
Dhiyauddin0210 <dhiyauddintjkt@gmail.com>
2026-07-15 11:31:58 +0700

    Add Render deployment config

---
+++ build.sh
@@ -0,0 +1,10 @@
+﻿#!/usr/bin/env bash
+set -o errexit
+composer install --optimize-autoloader --no-dev
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan migrate --force
+if [ ! -L "public/storage" ]; then
+    php artisan storage:link
+fi
